038275b3ea587bbb1012b2d75734b6643cb88067
Bob Schmitz <schmitzr1984@gmail.com>
2018-03-13 22:29:52 -0700

    Final draft
    Signed-off-by: Bob Schmitz <schmitzr1984@gmail.com>

--- src/TestGui.java
+++ src/TestGui.java
@@ -1,119 +1,15 @@
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Random;
-
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+import java.io.FileNotFoundException;
 
 public class TestGui {
-	private static String difficulty;
-	public static int SCORE;
-	public static int ATTEMPTS;
-
-	// Display final score for user
-	private static void finalScore() {
-		int overallScore = SCORE*100/ATTEMPTS;
-		String msg="Your final score is " + overallScore + "%, ";
-		if (overallScore >= 80) {
-			msg+="great job!";
-		} else {
-			msg+="keep studying...";
-		}
-		JLabel label = new JLabel(msg);
-		label.setHorizontalAlignment(JLabel.CENTER);
-		label.setVerticalTextPosition(JLabel.BOTTOM);
-		label.setHorizontalTextPosition(JLabel.CENTER);
-		JOptionPane.showMessageDialog(null, label, "Overall Score", JOptionPane.PLAIN_MESSAGE);
-
-	}
-	// Randomizer helper
-	private static void randomizer() {
-		ArrayList<File> files = selectDifficulty();
-		randomizer(files);
-	}
-	// Recursively selects a random quiz to run
-	private static void randomizer(ArrayList<File> files) {
-		Random rand = new Random();
-		int index = rand.nextInt(files.size());
-		File file = files.get(index);
-		files.remove(index);
-		try {
-			ReadYaml quiz = new ReadYaml("/"+difficulty+"/"+file.getName());
-			switch (quiz.getType()) {
-			case 1 :  // Multiple Choice Question
-				MultipleChoice question = new MultipleChoice(quiz);
-				if (quiz.checkAnswer(question.multipleChoiceWindow())) {
-					SCORE++;
-				}
-				break;
-			case 2 :  // Input Box Question
-				InputBox question2 = new InputBox(quiz);
-				if (quiz.checkAnswer(question2.inputBoxWindow())) {
-					SCORE++;
-				}
-				break;
-			case 3 :  // Check Box Question
-				CheckBox question3 = new CheckBox(quiz);
-				if (quiz.checkAnswer(question3.checkBoxWindow())) {
-					SCORE++;
-				}
-				break;
-			default :  // Random Expression Generator
-				Expression question4 = new Expression();
-				if ((difficulty.equals("cs143") ? question4.eval(9) : question4.eval(7))) {
-					SCORE++;
-				}
-			}
-			ATTEMPTS++;
-			if (files.size() == 0)  {
-				finalScore();
-			} else {
-				randomizer(files);
-			}
-		} catch (Exception e) {
-			finalScore();
-		}
-	}
-	// Select a difficulty level
-	private static ArrayList<File> selectDifficulty() {
-		DifficultySelection lvl = new DifficultySelection();
-		File dir=null;
-		switch (lvl.DifficultySelectionWindow()) {
-		case 1:
-			difficulty="cs142";
-			break;
-		case 2:		
-			difficulty="cs143";
-			break;
-		}
-		ArrayList<File> tmp= new ArrayList<>();
-		dir = new File("resources/"+difficulty);
-		for (File f: dir.listFiles()) tmp.add(f); 
-		return tmp;
-	}
 
 	//	public static void main(String[] args) {
 	public static void main(String[] args) {
+		API api = new API();
 		// Kick off quizzes
-		//		randomizer();
-
-		// TODO Remove this comment and all code below when deployed
-		// Insert your test quiz below
-		//		ReadYaml quiz = new ReadYaml("/cs143/Collections.yml");
-		ArrayList<File> files = selectDifficulty();
-		for (File file : files) {
-			System.out.println("/"+difficulty+"/"+file.getName()+"\n----");
-			ReadYaml quiz = new ReadYaml("/"+difficulty+"/"+file.getName());
-			if (quiz.getType() == 1) {
-				MultipleChoice question = new MultipleChoice(quiz);
-				System.out.println(quiz.checkAnswer(question.multipleChoiceWindow()));
-			} else if (quiz.getType() == 2) {
-				InputBox question = new InputBox(quiz);
-				System.out.println(quiz.checkAnswer(question.inputBoxWindow()));
-			} else {
-				CheckBox question = new CheckBox(quiz);
-				System.out.println(quiz.checkAnswer(question.checkBoxWindow()));
-			}
+		try {
+			api.randomizer();
+		} catch (FileNotFoundException e) {
+			System.out.println("oops!");
 		}
 	}
 
